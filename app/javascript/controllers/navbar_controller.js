22397592dfb1b71df0bbc765c35e48ff9dd21a02
Nicolas Goarant <>
2026-05-12 14:56:54 +0200

    Vigil'Asso : navbar responsive avec hamburger mobile + footer responsive

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu", "iconOpen", "iconClose"]
+
+  toggle() {
+    this.menuTarget.classList.toggle("hidden")
+    this.iconOpenTarget.classList.toggle("hidden")
+    this.iconCloseTarget.classList.toggle("hidden")
+  }
+}
